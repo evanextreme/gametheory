8d58e4693a9274eeb98cab61613b222b5198c8f8
Evan Hirsh <4483135+evanextreme@users.noreply.github.com>
2020-02-06 11:56:03 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,22 +6,7 @@ https://github.com/pypa/sampleproject
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
-from os import path
-# io.open is needed for projects that support Python 2.7
-# It ensures open() defaults to text mode with universal newlines,
-# and accepts an argument to specify the text encoding
-# Python 3 only projects can skip this import
-from io import open
-
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
-
+  
 setup(
     name='JustAGame',
     version='1.0dev',
