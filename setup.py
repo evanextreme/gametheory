d99d6139f731bcea3c6691227914d9462c85165d
Evan Hirsh <evan@ehirsh.com>
2020-04-09 16:23:14 -0400

    added snake and mattpat

--- setup.py
+++ setup.py
@@ -9,13 +9,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='gametheory',
-    version='1.0p2',
+    version='1.0p3',
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    package_data={'gametheory.audio': ['*.wav']},
+    include_package_data=True,
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     python_requires='>=3.6',
-    install_requires=['cryptography', 'termcolor'],
+    install_requires=['cryptography', 'playsound'],
     extras_require={  # Optional
         'dev': ['check-manifest'],
         'test': ['coverage'],
